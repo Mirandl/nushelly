23837f2e6cdea3a29f92316f391d93695904d958
Mirandl <1397105863@qq.com>
2023-06-30 11:22:45 +0800

    vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  devServer:{
+    host:'localhost',
+        port: 8080,
+    // 代理是更为简单安全的方式
+    proxy:{
+        '/api': {
+        //  代理跨域的如果任务请求不同，直接改这里
+        target:'http://mall-pre.springboot.cn',
+        changeOrigin:true,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    }
+  },
+  // publicPath:'/app',
+  // outputDir:'dist',
+  // indexPath:'index2.html',
+  // lintOnSave:false,
+  productionSourceMap:true,
+  chainWebpack:(config)=>{
+    config.plugins.delete('prefetch');
+  }
+}
